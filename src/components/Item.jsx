1f92c9da92a9b30adf6725d03ad6053d6b11c19f
Hazar NAMDAR <hazarnamdarrr@gmail.com>
2024-06-14 01:24:23 +0300

    Rendered the list components with props

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,12 +1,18 @@
-const Item = () => {
+const Item = props => {
+  const { pizza } = props;
+  const { name, ingredients, price, imageUrl } = pizza;
+
+  const imgAlt = imageUrl.split('/')[1].split('.')[0];
+
   return (
-    <section>
-      <img src='pizzas/spinaci.jpg' alt='spinaci' />
-      <h3>Spinaci</h3>
-      <p>
-        Tomato, mozzarella, spinach, egg, black pepper, garlic, and parmesan.
-      </p>
-    </section>
+    <li className='pizza'>
+      <img src={imageUrl} alt={imgAlt} />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>{price}</span>
+      </div>
+    </li>
   );
 };
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,28 @@
 import Item from './Item';
 
+import data from '../pizzaMenu.json';
+
 const Menu = () => {
+  const pizzas = data.pizzaMenu;
+
   return (
     <section className='menu'>
       <h2>Our menu</h2>
-      <Item />
-      <Item />
-      <Item />
+      <ul className='pizzas'>
+        {pizzas.map((pizza, i) => {
+          return (
+            <Item
+              key={i}
+              pizza={{
+                name: pizza.name,
+                ingredients: pizza.ingredients,
+                price: pizza.price,
+                imageUrl: pizza.photoName,
+              }}
+            />
+          );
+        })}
+      </ul>
     </section>
   );
 };
