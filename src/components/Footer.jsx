05378a9c08948f89717bd08eebf9301165721c90
Hazar NAMDAR <hazarnamdarrr@gmail.com>
2024-06-14 01:56:59 +0300

    Added conditional rendering to the footer and the menu components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,26 @@
 const Footer = () => {
+  const opensAt = 0;
+  const closesAt = 0;
+
+  const currentTime = new Intl.DateTimeFormat('tr-TR', {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+  }).format(new Date());
+  const currentHour = parseInt(currentTime.split(':')[0]);
+
+  const isOpen = currentHour >= opensAt && currentHour < closesAt;
+
   return (
     <footer>
-      <p>{new Date().toLocaleTimeString()}, We're currently open</p>
+      {isOpen ? (
+        <div className='order'>
+          <p>{currentTime}, We're currently open</p>
+          <button className='btn'>Order</button>
+        </div>
+      ) : (
+        <p>{currentTime}, We're currently closed</p>
+      )}
     </footer>
   );
 };
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,21 +8,26 @@ const Menu = () => {
   return (
     <section className='menu'>
       <h2>Our menu</h2>
-      <ul className='pizzas'>
-        {pizzas.map((pizza, i) => {
-          return (
-            <Item
-              key={i}
-              pizza={{
-                name: pizza.name,
-                ingredients: pizza.ingredients,
-                price: pizza.price,
-                imageUrl: pizza.photoName,
-              }}
-            />
-          );
-        })}
-      </ul>
+
+      {pizzas.length > 0 ? (
+        <ul className='pizzas'>
+          {pizzas.map((pizza, i) => {
+            return (
+              <Item
+                key={i}
+                pizza={{
+                  name: pizza.name,
+                  ingredients: pizza.ingredients,
+                  price: pizza.price,
+                  imageUrl: pizza.photoName,
+                }}
+              />
+            );
+          })}
+        </ul>
+      ) : (
+        <p>No pizzas here to display the menu yet. </p>
+      )}
     </section>
   );
 };
